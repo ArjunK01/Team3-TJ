6a479e9b64bc8ec5ce15bc9af3e932786ffa6f87
ArjunK01 <arjunkumar01@gmail.com>
2021-05-31 17:00:15 -0400

    update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const db = require("./firebase");
-const { v4: uuidv4 } = require("uuid");
 
 const PORT = 8000;
 
