4faad2b0b4d493ea00a6b8b2974288fbec6a8442
ArjunK01 <arjunkumar01@gmail.com>
2021-05-31 16:47:04 -0400

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/teacherdirectory">
+            {
+              //Change to component
+            }
+            <p>teacher directory</p>
+          </Route>
+          <Route path="/studentdirectory">
+            {
+              //Change to component
+            }
+            <p>student directory</p>
+          </Route>
+          <Route path="/calendar">
+            {
+              //Change to component
+            }
+            <p>calendar</p>
+          </Route>
+          <Route path="/class">
+            {
+              //Change to component
+            }
+            <p>class page</p>
+          </Route>
+          <Route path="/dashboard">
+            {
+              //Change to component
+            }
+            <p>dashboard</p>
+          </Route>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
