919dbe0e8a72595f967422a6bcd30f604590faef
Kathy Kang <kck3due@virginia.edu>
2021-06-01 10:10:37 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider";
+import Navigation from "./components/Navigation.js";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <Router>
-          <Switch>
-            <Route path="/teacherdirectory">
-              {
-                //Change to component
-              }
-              <p>teacher directory</p>
-            </Route>
-            <Route path="/studentdirectory">
-              {
-                //Change to component
-              }
-              <p>student directory</p>
-            </Route>
-            <Route path="/calendar">
-              {
-                //Change to component
-              }
-              <p>calendar</p>
-            </Route>
-            <Route path="/class">
-              {
-                //Change to component
-              }
-              <p>class page</p>
-            </Route>
-            <Route path="/dashboard">
-              {
-                //Change to component
-              }
-              <p>dashboard</p>
-            </Route>
-            <Route path="/"></Route>
-          </Switch>
-        </Router>
+        <Navigation />
       </AuthProvider>
     </div>
   );
