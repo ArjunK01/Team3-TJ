539cd5dcdb2c8a8f798c6e5419b924049f2e1223
ArjunK01 <arjunkumar01@gmail.com>
2021-06-01 08:52:42 -0400

    auth Context

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/teacherdirectory">
-            {
-              //Change to component
-            }
-            <p>teacher directory</p>
-          </Route>
-          <Route path="/studentdirectory">
-            {
-              //Change to component
-            }
-            <p>student directory</p>
-          </Route>
-          <Route path="/calendar">
-            {
-              //Change to component
-            }
-            <p>calendar</p>
-          </Route>
-          <Route path="/class">
-            {
-              //Change to component
-            }
-            <p>class page</p>
-          </Route>
-          <Route path="/dashboard">
-            {
-              //Change to component
-            }
-            <p>dashboard</p>
-          </Route>
-          <Route path="/"></Route>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route path="/teacherdirectory">
+              {
+                //Change to component
+              }
+              <p>teacher directory</p>
+            </Route>
+            <Route path="/studentdirectory">
+              {
+                //Change to component
+              }
+              <p>student directory</p>
+            </Route>
+            <Route path="/calendar">
+              {
+                //Change to component
+              }
+              <p>calendar</p>
+            </Route>
+            <Route path="/class">
+              {
+                //Change to component
+              }
+              <p>class page</p>
+            </Route>
+            <Route path="/dashboard">
+              {
+                //Change to component
+              }
+              <p>dashboard</p>
+            </Route>
+            <Route path="/"></Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
