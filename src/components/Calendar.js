6d5825310fbd90457fd193b977be15294c7254bd
JakeMondschein1 <jhm4by@virginia.edu>
2021-06-02 11:29:51 -0400

    Update Calendar.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,42 +1,58 @@
-import React from "react";
-
+import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
 
 export default function Calendar() {
-  const events = [{ title: "today's event", date: new Date() }];
-//   const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
 
-//     const getEvents = () => { // get events from the firebase dashboard in order to add to the calendar
-//         fetch('http://localhost:8000') // need server for events
-//         .then((resp) => {
-//             return resp.json();
-//         })
-//         .then((obj) => {
-//             setEvents(obj)
-//         })
-//         console.log(events)
-//     }
-
-//     const handleChange = (e) => {
-//         setEvents(e.target.value)
-//     }
-
-
+  const getEvents = async() => {
+      fetch('http://localhost:8000/events')
+      .then(async(res) => {
+        const results = await res.json();
+        let arr = [];
+        results.forEach((result) => {
+          arr.push({
+            title: result.description,
+            start: getDate(result.date)
+          })
+        })
+        setEvents(arr);
+      })
+    
+ }
 
 
+ function getDate(dayString) {
+  const today = new Date();
+  const year = today.getFullYear().toString();
+  let month = (today.getMonth() + 1).toString();
+
+  if (month.length === 1) {
+    month = "0" + month;
+  }
+
+  return dayString.replace("YEAR", year).replace("MONTH", month);
+}
+
+
+  
   return (
-    <div className="App">
-        {/* <div>
-            <button>
-
-            </button>
-        </div> */}
-      <FullCalendar
-        defaultView="dayGridMonth"
-        plugins={[dayGridPlugin]}
-        events={events}
-      />
-    </div>
-  );
+      <div style={{textAlign:"center"}} className="App">
+        <button onClick={getEvents}>
+          Click Here To Get Events
+        </button>
+        <FullCalendar
+          defaultView="dayGridMonth"
+          header={{
+            left: "prev,next",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay"
+          }}
+          plugins={[dayGridPlugin, timeGridPlugin]}
+          events={events}
+        />
+      </div>
+    );
 }
